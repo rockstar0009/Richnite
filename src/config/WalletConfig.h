12b6b65e026ccb9c1170329f9f8374d99951f1c1
Maayan <jjksvlogs@gmail.com>
2019-06-16 10:08:37 -0400

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, The TurtleCoin Developers
 // Copyright (c) 2018, The DeroGold Association
 // Copyright (c) 2019, The TurtleGold Developers
-// 
+// Copyright (c) 2019, The Richnite Developers
 // Please see the included LICENSE file for more information.
 
 #pragma once
